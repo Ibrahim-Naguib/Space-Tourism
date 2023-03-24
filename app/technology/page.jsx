a1d0fcbb13b2f94997431b362ac2c9facdc6b39f
Ibrahim Naguib <80571858+Ibrahim-Naguib@users.noreply.github.com>
2023-03-24 14:56:20 +0200

    Update page.jsx

--- app/technology/page.jsx
+++ app/technology/page.jsx
@@ -30,11 +30,13 @@ const Technology = () => {
             className="lg:hidden mx-auto"
             src={data.technology[tech].images.landscape}
             alt={data.technology[tech].name}
+            priority
           />
           <Image
             className="hidden lg:block"
             src={data.technology[tech].images.portrait}
             alt={data.technology[tech].name}
+            priority
           />
         </motion.div>
         <div className="mt-[35px] lg:mt-[100px] lg:order-1 lg:ml-[35px] lg:flex">
